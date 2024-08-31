d185bf03eb8da9b9898ea73bb6eff45d1a31e9c4
bwrob <bw2002@wp.pl>
2024-08-31 22:34:12 +0200

    vscode settings

---
+++ create_vscode_symlinks.zsh
@@ -0,0 +1,17 @@
+#!/home/linuxbrew/.linuxbrew/bin/zsh
+
+# Get the repository name and settings flavor as arguments
+echo "Enter a source file name:"
+read repo_name
+echo "Enter a settings flavor:"
+read settings_flavour
+
+src_settings="/home/bwrob/repos/dotfiles/.vscode.$settings_flavour/*.json"
+dest_settings="/home/bwrob/repos/$repo_name/.vscode/"
+echo "Creating symbolic links for $src_settings in $dest_settings"
+
+# Create the .vscode folder if it doesn't exist
+mkdir -p $dest_settings
+
+# Create symbolic links for each JSON file
+ln -Pf $src_settings $dest_settings
