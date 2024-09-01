84787f313bb9ce8af692b2b86c926be052803f5f
bwrob <bw2002@wp.pl>
2024-09-01 14:03:37 +0200

    zshrc

---
+++ .aliases.zsh
@@ -0,0 +1,8 @@
+# Aliases
+alias dir.all="eza -l --group-directories-first --icons --git-ignore --all"
+alias dir.git="eza -l --group-directories-first --icons"
+alias dir.tree="eza -l --group-directories-first --icons --git-ignore --tree --level 2"
+alias dir="eza -l --group-directories-first --icons --git-ignore"
+alias zsh.apply='source ~/.zshrc'
+alias zsh.config='nano ~/.zshrc'
+alias zsh:='which zsh'
