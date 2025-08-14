3fe25a8bb1315c14dcbbc2c7da39dc1b1a332047
bwrob <bartosz.marcin.wroblewski@gmail.com>
2025-08-14 23:50:49 +0200

    clean up setup for terminal

--- scripts/wsl_setup.sh
+++ scripts/wsl_setup.sh
@@ -10,51 +10,10 @@ echo "--- Installing Homebrew ---"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && brew doctor
 echo 'export XDG_DATA_DIRS="/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"' >> ~/.profile
 
-# Install ZSH
-brew install zsh
-zsh --version
-zsh
-
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/bwrob/.zshrc
 echo $SHELL &&  sudo chsh -s $(which zsh) &&  echo $SHELL
 
-# Install GitHub CLI
-echo "--- Installing GitHub CLI ---"
-brew install gh
-gh auth login
-
-# Install Rust
-echo "--- Installing Rust ---"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Install Haskell
-echo "--- Installing Haskell ---"
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-cabal install stylish-haskell
-
-# Install Python
-echo "--- Installing Python setup ---"
-brew install python3
-python3 --version
-
-brew install pipx
-pipx ensurepath
-
-pipx install argcomplete --force
 echo 'autoload -U compinit && compinit' >> ~/.zshrc
-echo 'eval "$(register-python-argcomplete pipx)"' >> ~/.zshrc
-
-pipx install poetry poethepoet pre-commit pytest ruff basedpyright hapless
-
-# Install Quarto
-echo "--- Installing Quarto ---"
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.5.57/quarto-1.5.57-linux-amd64.tar.gz
-mkdir ~/.local/share/quarto
-tar -C ~/.local/share/quarto -xvzf quarto-1.5.57-linux-amd64.tar.gz
-ln -s ~/.local/share/quarto/quarto-1.5.57/bin/quarto ~/.local/bin/quarto
-quarto install tinytex
-quarto install chromium
-quarto check
 
 # Install other tools
 echo "--- Installing other tools ---"
@@ -65,8 +24,19 @@ brew install \
     neovim \
     fastfetch  \
     oh-my-posh \
-    thefuck \
     tmux \
     zoxide \
     zsh-autosuggestions \
-    zsh-syntax-highlighting
+    zsh-syntax-highlighting \
+    gh \
+    yazi \
+    uv \
+    quarto \
+
+uv tool install poethepoet pre-commit pytest ruff basedpyright hapless
+
+gh auth login
+# Install Quarto
+quarto install tinytex
+quarto install chromium
+quarto check
